04d1d02bd98182f84e29a0e8541c2d020214e258
Benedict <benedictryan80@gmail.com>
2023-07-22 19:39:49 +0700

    omit edge function

--- pages/api/apify-scrape-igprofile/[username].js
+++ pages/api/apify-scrape-igprofile/[username].js
@@ -5,14 +5,6 @@ const client = new ApifyClient({
 	token: process.env.NEXT_APIFY_API_TOKEN
 });
 
-export const config = {
-	runtime: 'edge', // for Edge API Routes only
-	unstable_allowDynamic: [
-		'/lib/utilities.js', // allows a single file
-		'/node_modules/function-bind/**' // use a glob to allow anything in the function-bind 3rd party module
-	]
-};
-
 const handler = async (req, res) => {
 	// console.log(req.query);
 
