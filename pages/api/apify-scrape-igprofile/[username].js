d68d86e1d14261730411cbf30fda7d863b2211d6
Benedict <benedictryan80@gmail.com>
2023-07-22 19:38:45 +0700

    fix edge function

--- pages/api/apify-scrape-igprofile/[username].js
+++ pages/api/apify-scrape-igprofile/[username].js
@@ -6,7 +6,11 @@ const client = new ApifyClient({
 });
 
 export const config = {
-	runtime: 'edge' // this is a pre-requisite
+	runtime: 'edge', // for Edge API Routes only
+	unstable_allowDynamic: [
+		'/lib/utilities.js', // allows a single file
+		'/node_modules/function-bind/**' // use a glob to allow anything in the function-bind 3rd party module
+	]
 };
 
 const handler = async (req, res) => {
